fa2d776cb0fe0d10de2047bba4a2447698fef058
Loraine <loraine@lorainekv.com>
2018-05-17 00:39:25 -0700

    Fourth test passes.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,12 +2,21 @@ class Bottles
   def verse(count)
     <<-VERSE
 #{bottle_string(count)} of beer on the wall, #{bottle_string(count)} of beer.
-Take one down and pass it around, #{bottle_string(count - 1)} of beer on the wall.
+Take #{pronoun(count - 1)} down and pass it around, #{bottle_string(count - 1)} of beer on the wall.
     VERSE
   end
 
+  def pronoun(count)
+    if count == 0
+      "it"
+    else
+      "one"
+    end
+  end
   def bottle_string(count)
-    if count == 1
+    if count == 0
+      "no more bottles"
+    elsif count == 1
       "1 bottle"
     else
       "#{count} bottles"
