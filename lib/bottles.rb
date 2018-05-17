7bbc154e16bd1f1569b73d265eba7a1cfa2358c3
Loraine <loraine@lorainekv.com>
2018-05-17 00:35:18 -0700

    Three passing tests.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,16 @@
 class Bottles
   def verse(count)
     <<-VERSE
-#{count} bottles of beer on the wall, #{count} bottles of beer.
-Take one down and pass it around, #{count - 1} bottles of beer on the wall.
+#{bottle_string(count)} of beer on the wall, #{bottle_string(count)} of beer.
+Take one down and pass it around, #{bottle_string(count - 1)} of beer on the wall.
     VERSE
+  end
+
+  def bottle_string(count)
+    if count == 1
+      "1 bottle"
+    else
+      "#{count} bottles"
+    end
   end
 end
